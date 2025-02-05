288e5b8ca921b2388c478f1b38b1c040c4b2cf2b
minyekim <biie2004@naver.com>
2025-02-05 17:59:30 +0900

    Refactor : parameter개수, 채널의 구문, 채널의 존재에 대한 예외처리를 메서드로 분리

--- command/Part.hpp
+++ command/Part.hpp
@@ -7,6 +7,9 @@ class Part : public CommandHandler {
 	public:
 		Part();
 		~Part();
+		bool isParamCountValid(Client* sender, const Message& command, ServerEventHandler *server, int minRequiredParams, const std::string& errorMessage);
+		bool verifyChannelSyntax(Client* sender, ServerEventHandler *server, const std::string& channelName, const std::string& errorMessage);
+		bool validateChannelExists(Client* sender, std::map<std::string, Channel*>& channels, ServerEventHandler *server, std::string& channelName, const std::string& errorMessage);
 		void execute(Client* sender, const Message& command, std::map<int, Client*> &clients, std::map<std::string, Channel*>& channels, Auth &auth, ServerEventHandler *server);
 };
 
